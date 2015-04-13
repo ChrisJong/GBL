0f5bdf68517ff170ab8cf46cfe7536f98235e609
Giles Excell <9713395@student.swin.edu.au>
2015-04-13 18:15:07 +1000

    Camera Improvements

--- Galactic Battle League/Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
+++ Galactic Battle League/Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
@@ -40,6 +40,7 @@ function LateUpdate () {
 
 	// Damp the height
 	currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+	currentHeight = Mathf.Clamp (currentHeight, target.position.y, 40);
 
 	// Convert the angle into a rotation
 	var currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
