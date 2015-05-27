1be0f0c7215b4d156c34ce6b82caf405f200a37f
GilesExcell <9713395@student.swin.edu.au>
2015-05-27 22:41:53 +1000

    Crosshairs, some XInput fixes

---
+++ Galactic Battle League/Assets/Scripts/GuiCrosshair.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiCrosshair : MonoBehaviour {
+	public GameObject playerCameraHeavy;
+	public GameObject playerCameraLight;
+	public Transform worldCrosshair;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerCameraHeavy.activeInHierarchy) {
+			transform.position = playerCameraHeavy.GetComponent<Camera>().WorldToScreenPoint(worldCrosshair.position);
+		} else {
+			transform.position = playerCameraLight.GetComponent<Camera>().WorldToScreenPoint(worldCrosshair.position);
+		}
+	}
+}
