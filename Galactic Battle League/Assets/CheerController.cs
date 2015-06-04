7f090be57f0de0b071a9fefd4cd1ce78118cd08d
GilesExcell <9713395@student.swin.edu.au>
2015-06-04 11:15:27 +1000

    CheerController, UI prefabs
    Added a cheercontroller,
    updated the UI prefab

---
+++ Galactic Battle League/Assets/CheerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheerController : MonoBehaviour {
+	public float lowVolume = 0.1f;
+	public float highVolume = 0.3f;
+	private float cheerTime;
+	public AudioSource cheerSFX;
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > cheerTime)
+		{
+			cheerSFX.volume = lowVolume;
+		}
+	}
+
+	void Cheer () {
+		cheerTime = Time.time + 1.0f;
+		cheerSFX.volume = highVolume;
+	}
+}
