c65f2753ac59cb9fce0d4079c23d1f53f0c53b76
chloemitterer <9725725@student.swin.edu.au>
2015-06-12 15:22:22 +1000

    Empty AIController Class
    Added basic AIController class with variables and empty functions

---
+++ Galactic Battle League/Assets/Scripts/AIController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+	public GameObject player1Heavy;
+	public GameObject player1Light;
+	public GameObject player2Heavy;
+	public GameObject player2Light;
+	public GameObject player3Heavy;
+	public GameObject player3Light;
+	public GameObject player4Heavy;
+	public GameObject player4Light;
+
+	public float moveX;
+	public float moveY;
+	public float turn;
+	public bool fire;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update (int playerNumber) {
+	
+	}
+
+	// Resets the variables back to original values
+	void Reset () {
+
+	}
+}
