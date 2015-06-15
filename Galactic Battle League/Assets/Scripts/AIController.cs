ba947d09ff642721aa766ac2f69377819e31a3fd
GilesExcell <9713395@student.swin.edu.au>
2015-06-15 11:53:28 +1000

    Wander disabled, Open space seek
    AI Agents now seek open space by default, rather than wandering.
    All tanks have been set to be AI controlled

--- Galactic Battle League/Assets/Scripts/AIController.cs
+++ Galactic Battle League/Assets/Scripts/AIController.cs
@@ -21,10 +21,10 @@ public class AIController : MonoBehaviour {
 	public float wanderRadius = 2.0f;
 	public float wanderDistance = 2.2f;
 
-	Vector3 player1WanderTarget = Vector3.forward;
-	Vector3 player2WanderTarget = Vector3.forward;
-	Vector3 player3WanderTarget = Vector3.forward;
-	Vector3 player4WanderTarget = Vector3.forward;
+//	 Vector3 player1WanderTarget = Vector3.forward;
+//	 Vector3 player2WanderTarget = Vector3.forward;
+//	 Vector3 player3WanderTarget = Vector3.forward;
+//	 Vector3 player4WanderTarget = Vector3.forward;
 
 	// Use this for initialization
 	void Start () {
@@ -129,7 +129,11 @@ public class AIController : MonoBehaviour {
 			}		
 		} else {
 			// get wander vector
-			Vector3 direction = wander(self, playerNumber);
+			// Vector3 direction = wander(self, playerNumber);
+
+			// Super Wall Avoidance
+			Vector3 direction = OpenSpaceSeek(self);
+			direction = self.transform.InverseTransformDirection(direction);
 
 			// Wall avoidance
 			direction += wallAvoidance(self)*2;
@@ -139,8 +143,8 @@ public class AIController : MonoBehaviour {
 			moveX = direction.x;
 			moveY = direction.z;
 
-			// Turn so that it faces forwards always
-			turn = direction.x;
+			// Always be turning
+			turn = 0.5f;
 		}
 	}
 
@@ -205,53 +209,51 @@ public class AIController : MonoBehaviour {
 
 	}
 
-	// Returns a direction for wandering, local space, normalised
-	Vector3 wander(GameObject self, int playerNumber) {
-		// Get the correct player's wander target
-		Vector3 wanderTarget;
-		if (playerNumber == 1) {
-			wanderTarget = player1WanderTarget;
-		} else if (playerNumber == 2) {
-			wanderTarget = player2WanderTarget;
-		} else if (playerNumber == 3) {
-			wanderTarget = player3WanderTarget;
-		} else {
-			wanderTarget = player4WanderTarget;
-		}
-
-		wanderTarget.x += UnityEngine.Random.Range(-1.0f, 1.0f) * wanderJitter;
-		wanderTarget.z += UnityEngine.Random.Range(-1.0f, 1.0f) * wanderJitter;
-
-		// project onto unit circle
-		wanderTarget = wanderTarget.normalized;
-
-		// increase circle size
-		wanderTarget *= wanderRadius;
-
-		// Store the wander target for this player
-		if (playerNumber == 1) {
-			player1WanderTarget = wanderTarget;
-		} else if (playerNumber == 2) {
-			player2WanderTarget = wanderTarget;
-		} else if (playerNumber == 3) {
-			player3WanderTarget = wanderTarget;
-		} else if (playerNumber == 4) {
-			player4WanderTarget = wanderTarget;
-		}
-
-
-		// project in front of agent
-		wanderTarget.z += wanderDistance;
-
-		return wanderTarget.normalized;
-	}
+//	// Returns a direction for wandering, local space, normalised
+//	Vector3 wander(GameObject self, int playerNumber) {
+//		// Get the correct player's wander target
+//		Vector3 wanderTarget;
+//		if (playerNumber == 1) {
+//			wanderTarget = player1WanderTarget;
+//		} else if (playerNumber == 2) {
+//			wanderTarget = player2WanderTarget;
+//		} else if (playerNumber == 3) {
+//			wanderTarget = player3WanderTarget;
+//		} else {
+//			wanderTarget = player4WanderTarget;
+//		}
+//
+//		wanderTarget.x += UnityEngine.Random.Range(-1.0f, 1.0f) * wanderJitter;
+//		wanderTarget.z += UnityEngine.Random.Range(-1.0f, 1.0f) * wanderJitter;
+//
+//		// project onto unit circle
+//		wanderTarget = wanderTarget.normalized;
+//
+//		// increase circle size
+//		wanderTarget *= wanderRadius;
+//
+//		// Store the wander target for this player
+//		if (playerNumber == 1) {
+//			player1WanderTarget = wanderTarget;
+//		} else if (playerNumber == 2) {
+//			player2WanderTarget = wanderTarget;
+//		} else if (playerNumber == 3) {
+//			player3WanderTarget = wanderTarget;
+//		} else if (playerNumber == 4) {
+//			player4WanderTarget = wanderTarget;
+//		}
+//
+//
+//		// project in front of agent
+//		wanderTarget.z += wanderDistance;
+//
+//		return wanderTarget.normalized;
+//	}
 
 	// wall avoidance direction vector, local space, normalised
 	Vector3 wallAvoidance(GameObject self) {
 		// Feeler intersections
-		RaycastHit forwardHit;
-		RaycastHit leftHit;
-		RaycastHit rightHit;
+		RaycastHit hit;
 
 		// default values for no hits on feelers
 		float minDistance = Mathf.Infinity;
@@ -259,29 +261,29 @@ public class AIController : MonoBehaviour {
 		bool hitWall = false;
 
 		// forward feeler
-		if (Physics.Raycast(self.transform.position, self.transform.forward, out forwardHit, 10.0f)) {
+		if (Physics.Raycast(self.transform.position, self.transform.forward, out hit, 10.0f)) {
 			// if it's the shortest distance, it's the most important
-			if (forwardHit.distance < minDistance) {
-				minDistance = forwardHit.distance;
-				minHit = forwardHit;
+			if (hit.distance < minDistance) {
+				minDistance = hit.distance;
+				minHit = hit;
 				hitWall = true;
 			}
 		}
 		// left feeler
-		if (Physics.Raycast(self.transform.position, self.transform.forward - self.transform.right, out leftHit, 8.0f)) {
+		if (Physics.Raycast(self.transform.position, self.transform.forward - self.transform.right, out hit, 8.0f)) {
 			// if it's the shortest distance, it's the most important
-			if (leftHit.distance < minDistance) {
-				minDistance = leftHit.distance;
-				minHit = leftHit;
+			if (hit.distance < minDistance) {
+				minDistance = hit.distance;
+				minHit = hit;
 				hitWall = true;
 			}
 		}
 		// right feeler
-		if (Physics.Raycast(self.transform.position, self.transform.forward + self.transform.right, out rightHit, 8.0f)) {
+		if (Physics.Raycast(self.transform.position, self.transform.forward + self.transform.right, out hit, 8.0f)) {
 			// if it's the shortest distance, it's the most important
-			if (rightHit.distance < minDistance) {
-				minDistance = rightHit.distance;
-				minHit = rightHit;
+			if (hit.distance < minDistance) {
+				minDistance = hit.distance;
+				minHit = hit;
 				hitWall = true;
 			}
 		}
@@ -289,6 +291,109 @@ public class AIController : MonoBehaviour {
 		// if a feeler hits a wall, return local space normal direction, else return a zero vector 
 		if (hitWall) {
 			return self.transform.InverseTransformDirection(minHit.normal);
+		} else {
+			return Vector3.zero;
+		}
+	}
+
+	// wall avoidance direction vector, world space, normalised
+	Vector3 OpenSpaceSeek(GameObject self) {
+		RaycastHit hit;
+		Vector3 rayOrigin = self.transform.position;
+		rayOrigin.y += 1.4f;
+
+		// default values for no hits on feelers
+		float maxDistance = 0.0f;
+		RaycastHit maxHit = new RaycastHit();
+		bool hitWall = false;
+		Vector3 avg = Vector3.zero;
+
+		// forward feeler
+		if (Physics.Raycast(rayOrigin, self.transform.forward, out hit)) {
+			// if it's the shortest distance, it's the most important
+			avg += hit.point;
+			if (hit.distance > maxDistance) {
+				maxDistance = hit.distance;
+				maxHit = hit;
+				hitWall = true;
+			}
+		}
+		// forward right feeler
+		if (Physics.Raycast(rayOrigin, self.transform.forward + self.transform.right, out hit)) {
+			// if it's the shortest distance, it's the most important
+			avg += hit.point;
+			if (hit.distance > maxDistance) {
+				maxDistance = hit.distance;
+				maxHit = hit;
+				hitWall = true;
+			}
+		}
+		// right feeler
+		if (Physics.Raycast(rayOrigin, self.transform.right, out hit)) {
+			// if it's the shortest distance, it's the most important
+			avg += hit.point;
+			if (hit.distance > maxDistance) {
+				maxDistance = hit.distance;
+				maxHit = hit;
+				hitWall = true;
+			}
+		}
+		// back right feeler
+		if (Physics.Raycast(rayOrigin, -self.transform.forward + self.transform.right, out hit)) {
+			// if it's the shortest distance, it's the most important
+			avg += hit.point;
+			if (hit.distance > maxDistance) {
+				maxDistance = hit.distance;
+				maxHit = hit;
+				hitWall = true;
+			}
+		}
+		// back feeler
+		if (Physics.Raycast(rayOrigin, -self.transform.forward, out hit)) {
+			// if it's the shortest distance, it's the most important
+			avg += hit.point;
+			if (hit.distance > maxDistance) {
+				maxDistance = hit.distance;
+				maxHit = hit;
+				hitWall = true;
+			}
+		}
+		// back left feeler
+		if (Physics.Raycast(rayOrigin, -self.transform.forward + -self.transform.right, out hit)) {
+			// if it's the shortest distance, it's the most important
+			avg += hit.point;
+			if (hit.distance > maxDistance) {
+				maxDistance = hit.distance;
+				maxHit = hit;
+				hitWall = true;
+			}
+		}
+		// left feeler
+		if (Physics.Raycast(rayOrigin, -self.transform.right, out hit)) {
+			// if it's the shortest distance, it's the most important
+			avg += hit.point;
+			if (hit.distance > maxDistance) {
+				maxDistance = hit.distance;
+				maxHit = hit;
+				hitWall = true;
+			}
+		}
+		// forward left feeler
+		if (Physics.Raycast(rayOrigin, self.transform.forward + -self.transform.right, out hit)) {
+			// if it's the shortest distance, it's the most important
+			avg += hit.point;
+			if (hit.distance > maxDistance) {
+				maxDistance = hit.distance;
+				maxHit = hit;
+				hitWall = true;
+			}
+		}
+
+		avg = avg / 8.0f;
+
+		// if a feeler hits a wall, return local space normal direction, else return a zero vector 
+		if (hitWall) {
+			return seek(self, avg);
 		} else {
 			return Vector3.zero;
 		}
