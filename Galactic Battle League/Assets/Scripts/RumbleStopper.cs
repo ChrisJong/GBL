2fa3333ae4506c2bd310e473f1ad89a9ad358118
GilesExcell <9713395@student.swin.edu.au>
2015-07-30 22:45:14 +1000

    RumbleStopper

---
+++ Galactic Battle League/Assets/Scripts/RumbleStopper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class RumbleStopper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		foreach (var device in InputManager.Devices) {
+			device.Vibrate(0.0f, 0.0f);
+		}
+	}
+
+	void Update(){
+		foreach (var device in InputManager.Devices) {
+			device.Vibrate(0.0f, 0.0f);
+		}
+	}
+}
