f4cc49cc9e7747f6ff971b92c31869008f7538fa
GilesExcell <9713395@student.swin.edu.au>
2015-07-30 23:41:00 +1000

    Revert "Revert "BROKEN controller based menus""
    This reverts commit 97e6fb05bf08a9be9b3a0c8ee5b0c03c7587776a.

---
+++ Galactic Battle League/Assets/Scripts/StartArenaButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class StartArenaButton : MonoBehaviour {
+	void Update() {
+		var inputDevice = InputManager.ActiveDevice;
+		if (inputDevice.Action1.WasPressed) {
+			Application.LoadLevel ("arena");
+		} else if (inputDevice.MenuWasPressed) {
+			Application.Quit();
+		}
+	}	
+}
