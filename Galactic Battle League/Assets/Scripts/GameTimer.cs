749e141f6f5de229e180d027aadbd5e901b8a3ff
Mark McInnes <7183690@student.swin.edu.au>
2015-08-14 16:05:58 +1000

    Added timer to game
    Timer starts at 3 minutes and descends until 0. Nothing happens at 0
    however, win conditions need to be added.

---
+++ Galactic Battle League/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+
+	public float time;
+	Text gameTime;
+
+	// Use this for initialization
+	void Start () {
+		time = 180;
+		gameTime = GetComponent<Text> ();
+		InvokeRepeating ("DecreasingTime", 1, 1);
+		gameTime.text = DisplayTime ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameTime.text = DisplayTime ();
+	}
+
+	void IncreasingTime()
+	{
+		time++;
+	}
+
+	void DecreasingTime()
+	{
+		if (time > 0)
+			time--;
+		else
+			time = 0;
+	}
+
+	string DisplayTime()
+	{
+		int minutes = (int)time / 60;
+		int seconds = (int)time % 60;
+
+		return string.Format ("{0:0}", minutes) + ":" + string.Format ("{0:00}", seconds);
+	}
+}
