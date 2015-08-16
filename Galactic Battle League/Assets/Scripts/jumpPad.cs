3fb1d93cec6cdf55d62ebb73c12cbf203f38d0fc
Chris Jong <chris_jong@live.com.au>
2015-08-16 23:11:28 +1000

    #Arena 03 Changes
    + Added In Jump Pads / Code
    + Lifted Up Platforms
    + Basic Layout
    - No Side Colliders In Place
    - Renamed From Arena 02 To 03, due to trello cards

---
+++ Galactic Battle League/Assets/Scripts/jumpPad.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class jumpPad : MonoBehaviour
+{
+    public float jumpHeight = 150.0f;
+    private BoxCollider collider;
+
+    void Awake()
+    {
+        collider = GetComponent<BoxCollider>() as BoxCollider;
+        collider.isTrigger = true;
+        collider.size = new Vector3(1000.0f, 500.0f, 1000.0f);
+        collider.center = new Vector3(0.0f, 300.0f, 0.0f);
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.tag == "Player")
+            obj.GetComponent<Rigidbody>().AddForce(transform.up * jumpHeight, ForceMode.VelocityChange);
+    }
+}
