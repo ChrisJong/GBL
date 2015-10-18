6e3762171bdcd473dc53afd3c743c36f4ce72a89
kerim <2058502@student.swin.edu.au>
2015-10-19 00:27:57 +1100

    New respawn towers
    Updated respawn tower mesh and animations

---
+++ Galactic Battle League/Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantRotation : MonoBehaviour 
+{
+
+	void Update ()
+	{
+		transform.Rotate (0,0,200*Time.deltaTime); //rotates 50 degrees per second around z axis
+	}
+
+}
